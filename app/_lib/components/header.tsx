08c074fd4e8f141097c2a8cfbf96d3cd06aaaa9b
Ryan Oliveira <ryanprofissional02@gmail.com>
2025-04-17 18:40:01 -0300

    feat: change theme when button is clicked

--- app/_lib/components/header.tsx
+++ app/_lib/components/header.tsx
@@ -3,27 +3,40 @@
 import { Sun, Moon } from "lucide-react";
 import Image from "next/image";
 import { Button } from "./ui/button";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Header() {
-  const [theme, setTheme] = useState<"dark" | "light">("dark");
+  const [theme, setTheme] = useState<"light" | "dark">("dark");
+
+  useEffect(() => {
+    const root = document.documentElement;
+    if (theme === "dark") {
+      root.classList.add("dark");
+    } else {
+      root.classList.remove("dark");
+    }
+  }, [theme]);
 
   const toggleTheme = () => {
-    setTheme((prevTheme) => (prevTheme === "dark" ? "light" : "dark"));
+    setTheme(theme === "dark" ? "light" : "dark");
   };
 
   return (
     <div className="flex items-center justify-between pb-6">
-      <div className="relative h-[26px] w-[330px]">
-        <Image src="/images/logo.svg" alt="Logo" fill priority />
+      <div className="flex items-center gap-1">
+        <div className="relative h-[20px] w-[23.32px]">
+          <Image src="/images/logo.svg" alt="Logo" fill priority />
+        </div>
+        <h1 className="font-medium">Simple Finance</h1>
       </div>
 
       <Button size="icon" variant="ghost" onClick={toggleTheme}>
         {theme === "dark" ? (
           <Sun color="white" size={20} />
         ) : (
-          <Moon color="white" size={20} />
+          <Moon color="black" size={20} />
         )}
+        <span className="sr-only">Alternar tema</span>
       </Button>
     </div>
   );
